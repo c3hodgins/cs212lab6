d22e805ab50b6d112e50b20da94388781d59a05a
c3hodgins <c3hodgins@gmail.com>
2024-04-05 16:24:24 -0400

    Ready for testing

--- HodginsC_Lab6.c
+++ HodginsC_Lab6.c
@@ -8,37 +8,42 @@ int main(){
 
     openDataFiles(&pInput,"Input.txt",&pOutput,"Output.txt");
 
+    PrintHeader(pOutput);
+
     stackType* stack = NULL;
 
-    if (pInput != NULL){
+    if (pInput != NULL){    
         //check if file opens correctly
-            while (fgets(string, 40, pInput) != EOF){
-                // newNode = (nodeType*)malloc(sizeof(nodeType));
-                // newNode->record.symbol = symbol;
-                // push(stack,newNode);
-                for (int i = 0;i <strlen(string);i++){
-                    if ((string[i] < 57)&&(string[i]>47)){
-                        fprintf(pOutput,"%c",string[i]);
-                    }else 
-                        if (string[i] = '('){
+        while (fgets(string, 40, pInput) != EOF){
+            for (int i = 0;i <strlen(string);i++){
+                if ((string[i] < 57)&&(string[i]>47)){
+                    fprintf(pOutput,"%c",string[i]);
+                }else{
+                    if (string[i] = '('){
+                        newNode = (nodeType*)malloc(sizeof(nodeType));
+                        newNode->record.symbol = string[i];
+                        push(stack,newNode);
+                    }else if (string[i]  == ')'){
+                        while((stack)->pTop->record.symbol != '('){
+                            fprintf(pOutput,"%c",stack->pTop->record.symbol);
+                            pop(stack);
+                        }
+                        pop(stack);
+                    }else if ((string[i] == '+')&&(string[i] == '-')&&(string[i] == '/')&&(string[i] == '*')){
+                        while(returnPriority(stack->pTop->record.symbol)>=returnPriority(string[i])){
+                            fprintf(pOutput,"%c",stack->pTop->record.symbol);
                             newNode = (nodeType*)malloc(sizeof(nodeType));
                             newNode->record.symbol = string[i];
                             push(stack,newNode);
-                        }else if (string[i]  == ')'){
-                            while((stack)->pTop->record.symbol != '('){
-                                printf(pOutput,"%c",stack->pTop->record.symbol);
-                                pop(stack);
-                            }
-                        }else if (string[i] == '+')
+                        }
+                    }
                 }
-
+                while(stack->pTop != NULL){
+                    fprintf(pOutput,"%c",stack->pTop);
+                    pop(stack->pTop);
+                }
+                fprintf(pOutput,"\n");
             }
-	}
-	else{
-		//print if file doesnt open corrrectly
-		printf("File open unsuccessful");	
-	}
-}
-
+        }
     }
 }
